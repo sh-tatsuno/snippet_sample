ca5a63bf165dd1951d94969cfebe7b2c89555c7a
ShuMatsuki <shu.matsuki.0625@gmail.com>
2022-02-22 17:32:57 +0900

    修正

--- js/pendo.js
+++ js/pendo.js
@@ -1,3 +1,6 @@
+var userArray = ["test1", "test2", "test3", "test4","test5", "test6", "test7", "test8","test9", "test10"]
+var nowUser = userArray[Math.floor(Math.random() * userArray.length)];
+
 (function(apiKey){
     (function(p,e,n,d,o){var v,w,x,y,z;o=p[d]=p[d]||{};o._q=o._q||[];
     v=['initialize','identify','updateOptions','pageLoad','track'];for(w=0,x=v.length;w<x;++w)(function(m){
@@ -15,17 +18,8 @@
         const s = ("00" + date.getSeconds()).slice(-2)
         return Y + M + D + h + m + s
       };
-
-      const userArray = ["test1", "test2", "test3", "test4","test5", "test6", "test7", "test8","test9", "test10"]
-          
-       let nowUser = userArray[Math.floor(Math.random() * userArray.length)];
         
-       const userIdDiv = document.getElementById('user-id');
-       if (userIdDiv) {
-           console.log(`今あなたは　${nowUser}です`)
-           userIdDiv.textContent = nowUser;
-       }
-
+        console.log(`今あなたは　${nowUser}です`)
         // Call this whenever information about your visitors becomes available
         // Please use Strings, Numbers, or Bools for value types.
         pendo.initialize({
