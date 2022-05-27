e51975fc906b20705d4b61a9055c47c49b534dc7
sho techtouch <shotechtouch@shos-MacBook-Pro.local>
2022-05-27 18:24:26 +0900

    tmp

--- js/cookieconsent/cookieconsent-init.js
+++ js/cookieconsent/cookieconsent-init.js
@@ -89,7 +89,7 @@ cc.run({
                 blocks: [
                     {
                         title: 'Cookie usage 📢',
-                        description: 'I use cookies to ensure the basic functionalities of the website and to enhance your online experience. You can choose for each category to opt-in/out whenever you want. For more details relative to cookies and other sensitive data, please read the full <a href="#" class="cc-link">privacy policy</a>.'
+                        description: 'I use cookies to ensure the basic functionalities of the website and to enhance your online experience. You can choose for each category to opt-in/out whenever you want. For more details relative to cookies and other sensitive data, please read the full <a href="https://sh-tatsuno.github.io/snippet_sample/policy.html" class="cc-link">privacy policy</a>.'
                     }, {
                         title: 'Strictly necessary cookies',
                         description: 'These cookies are essential for the proper functioning of my website. Without these cookies, the website would not work properly',
